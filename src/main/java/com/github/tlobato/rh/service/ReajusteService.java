5e1868be012019f8cf4a879caa919458cdc2c63a
Thyago Lobato <thyagollobato@gmail.com>
2023-03-02 15:58:01 -0300

    SRP - Single Responsibility Principle: Extraindo a lógica de reajuste salarial

---
+++ src/main/java/com/github/tlobato/rh/service/ReajusteService.java
@@ -0,0 +1,20 @@
+package com.github.tlobato.rh.service;
+
+import com.github.tlobato.rh.ValidacaoException;
+import com.github.tlobato.rh.model.Funcionario;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ReajusteService {
+
+    public void reajustarSalarioDoFuncinario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.atualizarSalario(salarioReajustado);
+    }
+
+}
