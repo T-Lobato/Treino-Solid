8ed38af70eb1183f139ed2eb4c54798c8f26787e
Thyago Lobato <thyagollobato@gmail.com>
2023-03-04 10:30:35 -0300

    DIP - Dependency Inversion Principle

--- src/main/java/com/github/tlobato/rh/service/ValidacaoReajuste.java
+++ src/main/java/com/github/tlobato/rh/service/ValidacaoReajuste.java
@@ -5,6 +5,13 @@ import java.math.BigDecimal;
 
 public interface ValidacaoReajuste {
 
+    /*
+    Com a utilização dessa interface para implementar o OCP - OPEN CLOSE PRINCIPLE, nós acabamos seguindo também
+    o príncipio DIP - DEPENDENCY INVERSION PRINCIPLE, a letra 'D' do Solid.
+    Pois dessa forma, sempre que for necessário fazer uma nova validação, basta criarmos uma nova classe
+    que implementa essa interface, sem a necessidade de mexer na classe ReajusteService.
+     */
+
     void validar(Funcionario funcionario, BigDecimal aumento);
 
 }
